831535e0d171d5038131ffe528be4dd18c26c4ba
Max <41769755+madyafaraz@users.noreply.github.com>
2018-10-26 02:48:34 -0400

    Add files via upload
    Jquery Demo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Show Images 
+$("#show").click(function() {
+	"use strict";
+    $("#gallery").show();
+	   
+    });
+
+//Hide Images
+$("#hide").click(function() {
+	"use strict";
+    $("#gallery").show();
+	   
+    });
